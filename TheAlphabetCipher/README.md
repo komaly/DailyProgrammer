8040be1323166f3feea693829219c02bb6aba85f
komaly <kyaseen@uci.edu>
2018-04-19 15:57:16 -0700

    First Challenge

--- TheAlphabetCipher/README.md
+++ TheAlphabetCipher/README.md
@@ -1,4 +1,4 @@
-This is an implementation of a Vigenère Cipher encrypt and decrypt functions. This is an answer to a Daily Programmer question included below.
+This is an implementation of a Vigenère Cipher (https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher) encryption and decryption. This is an answer to a Daily Programmer question included below.
 
-"The Alphabet Cipher", published by Lewis Carroll in 1868, describes a Vigenère cipher (thanks /u/Yadkee for the clarification) for passing secret messages. The cipher involves alphabet substitution using a shared keyword. Using the alphabet cipher to tranmit messages follows this procedure:
-...read more about... more about the problem at https://www.reddit.com/r/dailyprogrammer/comments/879u8b/20180326_challenge_355_easy_alphabet_cipher/
+"The Alphabet Cipher", published by Lewis Carroll in 1868, describes a Vigenère cipher for passing secret messages. The cipher involves alphabet substitution using a shared keyword. Using the alphabet cipher to transmit messages follows this procedure:
+...read more about the problem at https://www.reddit.com/r/dailyprogrammer/comments/879u8b/20180326_challenge_355_easy_alphabet_cipher/
--- TheAlphabetCipher/TheAlphabetCipher.py
+++ TheAlphabetCipher/TheAlphabetCipher.py
@@ -1,48 +1,92 @@
+'''
 
-def cipher(filename, type):
+Encrypt and Decrypt functions for the Vigenere Cipher
+Answer to Reddit's Daily Programmer Challenge #355 
+(Modified to include capital letters and other characters as inputs)
+
+'''
+#Return's a character's index in a shifted ASCII alphabet
+def charToCode(char):
+	o = ord(char)
+
+	if o < 32 or o > 126:
+		return 0
+
+	return o - 32
+
+#Returns a character from an index in a shifted ASCII alphabet
+def codeToChar(code):
+	return chr(code + 32)
+
+#Encrypt text character by character
+def encrypt(secretWord, plainText):
+	output = ""
+	keyIndex = 0
+	for letter in plainText:
+
+		#Add corresponding key character to encrypted output
+		output += codeToChar((charToCode(letter) + charToCode(secretWord[keyIndex]) ) % 95)
+		
+		#Increment the key index to go to the next character in the key
+		keyIndex = (keyIndex + 1) % len(secretWord)
+
+	print("PlainText: " + plainText + "\nEncrypted: " + output + "\n")
+	return output
+	
+#Decrypt text character by character	
+def decrypt(secretWord, cipherText):
+	output = ""
+	keyIndex = 0
+	for letter in cipherText:
+
+		#Add corresponding key character to decrypted output
+		output += codeToChar((charToCode(letter) - charToCode(secretWord[keyIndex]) ) % 95)
+
+		#Increment the key index to go to the next character in the key
+		keyIndex = (keyIndex + 1) % len(secretWord)
+	
+	print("Encrypted: " + cipherText + "\nPlainText: " + output + "\n")
+	return output
+
+#Opens the file with plaintext and encrypts it line by line.
+#Opens the file with encrypted text and decrypts it line by line.
+def main(encryptFile, decryptFile):
 	try:
-		file = open(filename, 'r')
+		file = open(encryptFile, 'r')
 	except:
-		print("Error opening file.")
+		print("Error opening encrypting file.")
 		return
 
-	alphabet = "abcdefghijklmnopqrstuvwxyz"
-
 	line = file.readline()
+	lineNum = 1
+	print("Encrypting...")
 	while line:
-		input = line.split()
-		keyLetter = input[0][0]
-		count = 1
-		output = ""
-		for letter in input[1]:
-			if type == "encrypt":
-				num = alphabet.index(letter) + alphabet.index(keyLetter)
-			elif type == "decrypt":
-				num = alphabet.index(letter) - alphabet.index(keyLetter)
-			else:
-				print("Invalid request.")
-				return
-
-			if num > 25 or num < 0:
-				output += alphabet[num % 26]
-			else:
-				output += alphabet[num]
-
-			keyLetter = input[0][count]
-			count += 1
-			if count == len(input[0]):
-				count = 0
-
-		
-		if type == "encrypt":
-			print("Original: " + input[1] + "\nEncrypted: " + output + "\n")
-		if type == "decrypt":
-			print("Original: " + input[1] + "\nDecrypted: " + output + "\n")
+		input = line.split(maxsplit = 1)
+		if len(input) < 2:
+			print("Error with line number " + lineNum)
+		else:
+			encrypt(input[0], input[1][:-1])
 
 		line = file.readline()
+		lineNum += 1
 
-print("Encrypted Phrases")
-cipher("input.txt", "encrypt")
-print("\n")
-print("Decrypted Phrases")
-cipher("decryptor_input.txt", "decrypt")
+	try:
+		file = open(decryptFile, 'r')
+	except:
+		print("Error opening decrypting file.")
+		return
+
+	line = file.readline()
+	lineNum = 1
+	print("Decrypting...")
+	while line:
+		input = line.split(maxsplit = 1)
+		if len(input) < 2:
+			print("Error with line number " + lineNum)
+		else:
+			decrypt(input[0], input[1][:-1])
+
+		line = file.readline()
+		lineNum += 1
+
+main("encryptor_input.txt", "decryptor_input.txt")
