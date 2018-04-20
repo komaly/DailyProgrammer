298110ce4448f41801135ee7d8b7d062b9208556
komaly <kyaseen@uci.edu>
2018-04-20 15:02:59 -0700

    Second Challenge

--- FindSmallestAdditionOfFactors/FindSmallestAdditionOfFactors.java
+++ FindSmallestAdditionOfFactors/FindSmallestAdditionOfFactors.java
@@ -1,38 +1,36 @@
 
 /**
-* Takes an input A and computes B and C values such that A = B * C. 
+* Takes an input A and computes factor pairs B and C such that A = B * C. 
 * Computes the smallest B + C value.
 * Answer to Reddit's Daily Programmer Challenge #354
 */
 
 public class FindSmallestAdditionOfFactors
 {
-	private int num;
+	private Long num;
 
-	public FindSmallestAdditionOfFactors(int num)
+	public FindSmallestAdditionOfFactors(Long num)
 	{
 		this.num = num;
 	}
 
 	/**
-	* Checks if the given number is divisible by a number (B) in the 
-	* range 1 to the square root of the given number. If it is,
-	* B is calculated, and if B + C is less than the current minimum
-	* then the minimum is updated to B + C.
+	* Computes the factor pairs of the given number and 
+	* determines the factor pair with the smallest value when the 2 
+	* numbers in the pair are added together.
 	*/
 	public void computeSmallestNumber()
 	{
-		int min = Integer.MAX_VALUE; 
-		for ( int B = 1; B < java.lang.Math.sqrt(num); B++)
+		long min = Integer.MAX_VALUE; 
+		for ( long B = 1; B < java.lang.Math.sqrt(num); B++)
 		{
 			if (num % B == 0)
 			{
-				int C = num / B;
+				long C = num / B;
 				if (B + C < min)
 					min = B + C;
 			}
 		}
-
 		System.out.println("Input: " + num + "\nOutput: " + min + "\n");
 	} 
 
@@ -42,16 +40,19 @@ public class FindSmallestAdditionOfFactors
 	*/
 	public static void main(String args[])
 	{
-		FindSmallestAdditionOfFactors f1 = new FindSmallestAdditionOfFactors(12);
+		FindSmallestAdditionOfFactors f1 = new FindSmallestAdditionOfFactors(Long.valueOf(12));
 		f1.computeSmallestNumber();
 
-		FindSmallestAdditionOfFactors f2 = new FindSmallestAdditionOfFactors(456);
+		FindSmallestAdditionOfFactors f2 = new FindSmallestAdditionOfFactors(Long.valueOf(456));
 		f2.computeSmallestNumber();
 
-		FindSmallestAdditionOfFactors f3 = new FindSmallestAdditionOfFactors(4567);
+		FindSmallestAdditionOfFactors f3 = new FindSmallestAdditionOfFactors(Long.valueOf(4567));
 		f3.computeSmallestNumber();
 
-		FindSmallestAdditionOfFactors f4 = new FindSmallestAdditionOfFactors(12345);
+		FindSmallestAdditionOfFactors f4 = new FindSmallestAdditionOfFactors(Long.valueOf(12345));
 		f4.computeSmallestNumber();
+
+		FindSmallestAdditionOfFactors f5 = new FindSmallestAdditionOfFactors(Long.valueOf(1234567891011L));
+		f5.computeSmallestNumber();
 	}
 }
