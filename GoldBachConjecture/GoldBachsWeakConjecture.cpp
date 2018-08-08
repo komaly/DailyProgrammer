5a2e05a76f1ffe377e1ec21f855f534609d3eb27
komaly <kyaseen@uci.edu>
2018-08-07 21:58:07 -0700

    GoldBackConjecture

---
+++ GoldBachConjecture/GoldBachsWeakConjecture.cpp
@@ -0,0 +1,136 @@
+/*
+This program takes an odd number and yields 3 prime numbers
+that sum up to the odd number, implementing Goldbach's weak 
+conjecture. The conjecture states that every odd number greater
+than 5 can be expressed as the sum of 3 prime numbers.
+*/
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+/*
+Checks if a number is prime.
+*/
+bool isPrime(int n)
+{
+	if (n < 2)
+	 	return false;
+
+	 if (n == 2)
+	 	return true;
+
+	 if (n % 2 == 0)
+	 	return false;
+ 
+    for (int i = 3; i <= sqrt(n); i+=2)
+        if (n % i == 0)
+            return false;
+ 
+    return true;
+}
+
+/*
+Saves all possible prime numbers to a file.
+*/
+void saveAllPrimes()
+{
+	int max = numeric_limits<int>::max();
+	std::ofstream o("primeNumbers.txt");
+
+	for (int i = 0; i <= max; i++)
+	{
+		if (isPrime(i))
+			o << i << endl;
+	}
+}
+
+/*
+Finds 3 prime numbers that sum up to the 
+given number.
+*/
+vector<int> findPrimes(int n)
+{
+	vector<int> l;
+	vector<int> output;
+
+	if (n % 2 == 0 || n < 5)
+		return output;
+
+	for (int i = 2; i < n; i++)
+	{
+		if (isPrime(i))
+			l.push_back(i);
+	}
+	
+	for (int j = 0; j < l.size(); j++)
+	{
+		for (int k = 0; k < l.size(); k++)
+		{
+			for (int m = 0; m < l.size(); m++)
+			{
+				if (l[j] + l[k] + l[m] == n)
+				{
+					output.push_back(l[j]);
+					output.push_back(l[k]);
+					output.push_back(l[m]);
+					return output;
+				}
+			}
+		}
+	}
+
+	return output;	
+}
+
+/*
+Prints out the given number and the 3 prime numbers
+that add up to it.
+*/
+void printOutput(vector<int> output, int n)
+{
+	if (output.size() == 0)
+	{
+		cout << "Error with " << n << ". Number must be odd number greater than 5." << endl;
+		return;
+	}
+
+	cout << n << " = ";
+	for (int i = 0; i < output.size(); i++)
+	{
+		if (i == output.size() - 1)
+			cout << output[i] << endl;
+		else
+	    	cout << output[i] << " + ";
+	}
+}
+
+
+int main(int argc, char const *argv[])
+{
+	//saveAllPrimes();
+
+	vector<int> output = findPrimes(11);
+	printOutput(output, 11);
+	
+	vector<int> output1 = findPrimes(35);
+	printOutput(output1, 35);
+	
+	vector<int> output2 = findPrimes(111);
+	printOutput(output2, 111);
+	
+	vector<int> output3 = findPrimes(17);
+	printOutput(output3, 17);
+	
+	vector<int> output4 = findPrimes(199);
+	printOutput(output4, 199);
+	
+	vector<int> output5 = findPrimes(287);
+	printOutput(output5, 287);
+	
+	vector<int> output6 = findPrimes(53);
+	printOutput(output6, 53);
+	return 0;
+}
---
+++ GoldBachConjecture/README.md
@@ -0,0 +1,12 @@
+According to Goldbach’s weak conjecture, every odd number greater than 5 can be expressed 
+as the sum of three prime numbers. (A prime may be used more than once in the same sum.)
+
+This program applies Goldbach's weak conjecture to numbers and shows which 3 primes, 
+added together, yield the result.
+
+Example:
+Input: 11
+Output: 2 + 2 + 7
+
+This challenge was taken from Reddits Daily Programming Challenge #356, found here:
+https://www.reddit.com/r/dailyprogrammer/comments/8bh8dh/20180411_challenge_356_intermediate_goldbachs/
